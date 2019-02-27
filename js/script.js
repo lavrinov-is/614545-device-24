1116d66e42e4e6885d43838fc79249f58af18fc4
Igor <igor.lavrinoff@yandex.ru>
2019-02-27 22:44:37 +0400

    Защита проекта

--- js/script.js
+++ js/script.js
@@ -20,6 +20,10 @@ var close = document.querySelector(".modal-close");
 var linkMap = document.querySelector(".contacts-map");
 var mapPopup = document.querySelector(".modal-map");
 var mapClose = document.querySelector(".modal-map-close");
+var form = document.querySelector(".form-modal");
+var login = document.querySelector("[name=name]");
+var email = document.querySelector("[name=email]");
+var formBtn = document.querySelector(".btn-submit");
 
 serviceBtnFirst.addEventListener("click", function (evt) {
 	evt.preventDefault();
@@ -93,11 +97,13 @@ sliderBtnThird.addEventListener("click", function (evt) {
 linkWriteUs.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.add("modal-show");
+	login.focus();
 });
 
 close.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error");
 });
 
 linkMap.addEventListener("click", function (evt) {
@@ -108,4 +114,21 @@ linkMap.addEventListener("click", function (evt) {
 mapClose.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	mapPopup.classList.remove("modal-show");
+});
+
+form.addEventListener("submit", function (evt) {
+	if (!login.value || !email.value) {
+		evt.preventDefault();
+		popup.classList.add("modal-error");
+	}
+});
+
+window.addEventListener("keydown", function (evt) {
+	if (evt.keyCode === 27) {
+		if (popup.classList.contains("modal-show") || mapPopup.classList.contains("modal-show")) {
+			evt.preventDefault();
+			popup.classList.remove("modal-show");
+			mapPopup.classList.remove("modal-show");
+		}
+	}
 });
